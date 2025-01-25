7c3e34cfccc687d189edddb2722ac6a0ef275548
liemvt2008 <liemvt2008@gmail.com>
2025-01-25 06:40:40 -0500

    implement list all accounts in service

--- tests/test_routes.py
+++ tests/test_routes.py
@@ -190,3 +190,26 @@ class TestAccountService(TestCase):
             content_type="test/html"
         )
         self.assertEqual(response.status_code, status.HTTP_415_UNSUPPORTED_MEDIA_TYPE)
+
+    def test_list_all_accounts(self):
+        """List: It should List all Accounts"""
+        account_count = 5
+        self._create_accounts(account_count)
+        response = self.client.get(f"{BASE_URL}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        response_data = response.get_json()
+        self.assertEqual(len(response_data), account_count)
+
+    def test_list_all_accounts_no_products_found(self):
+        """List: It should return success status when no account could be found"""
+        """
+        Note: It is deliberately programmed so that no error code (e.g. 404)
+          is sent, but a success code!
+          It is not an error if nothing specific was searched for
+          and nothing was found in an empty database.
+        """
+        response = self.client.get(f"{BASE_URL}")
+        self.assertEqual(response.status_code, status.HTTP_200_OK)
+        self.assertEqual(len(response.get_json()), 0)
+        # Just to be sure
+        self.assertEqual(len(Account.all()), 0)
